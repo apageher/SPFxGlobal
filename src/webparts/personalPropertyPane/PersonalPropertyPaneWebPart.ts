f53aa24b62c5558be728dc4605b7cf4b1d49195b
Álvaro Page <alvaro.page@raona.com>
2020-10-14 16:29:02 +0200

    Se añaden todos los tipos de controles, validaciones, mostrarlos condicionalmente y Localizarlos

--- src/webparts/personalPropertyPane/PersonalPropertyPaneWebPart.ts
+++ src/webparts/personalPropertyPane/PersonalPropertyPaneWebPart.ts
@@ -11,14 +11,23 @@ import {
   PropertyPaneSlider,
   PropertyPaneToggle,
   PropertyPaneLink,
+  PropertyPaneButton,
+  PropertyPaneButtonType,
   PropertyPaneHorizontalRule,
-  PropertyPaneChoiceGroup
+  PropertyPaneLabel,
+  PropertyPaneChoiceGroup,
+  IPropertyPaneGroup,
+  IPropertyPaneConditionalGroup,
+  IPropertyPaneTextFieldProps,
+  IPropertyPaneToggleProps,
+  IPropertyPaneField
 } from '@microsoft/sp-property-pane';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 
 import * as strings from 'PersonalPropertyPaneWebPartStrings';
 import PersonalPropertyPane from './components/PersonalPropertyPane';
 import { IPersonalPropertyPaneProps } from './components/IPersonalPropertyPaneProps';
+
 
 export interface IPersonalPropertyPaneWebPartProps {
   name: string;
@@ -33,14 +42,31 @@ export interface IPersonalPropertyPaneWebPartProps {
   aboutMe: string;
   layout: 'Brick' | 'Grid' | 'Carousel'; //Puede ser un entero o un enum
   shape: 'Circle' | 'Square' | 'Triangle'; //Puede ser un entero o un enum
+  conversationSource: 'Group' | 'User' | 'Topic' | 'Home';
+  searchCriteria: string;
+  numberOfConversations: number;
+  btnNormal: string;
+  btnPrimary: string;
+  btnHero: string;
+  btnCommand: string;
+  btnCompound: string;
+  btnIcon: string;
+  textoTest: string;
+  toggleAllEvents: boolean;
+
+  headerType:string;
+  title: string;
+  subTitle?: string;
+  enabled?: boolean;
+  descriptionCustom?: string;
 }
 
 //OK Añadir todos los tipos de propiedades
 //OK Usar validaciones
 //Añadir propiedades de PnP https://pnp.github.io/sp-dev-fx-property-controls
-//Usar fichero LOC
+//OK Usar fichero LOC
 //Crear un componente personalizado
-//¿Nombre del WP dependiendo del idioma?
+//OK Nombre del WP dependiendo del idioma
 
 // OK Choice group
 // OK Choice group de iconos
@@ -53,11 +79,10 @@ export interface IPersonalPropertyPaneWebPartProps {
 // OK Toggle
 // OK Horizontal rule
 // OK Multi-line Textbox
-// Button
+// OK Button
+// OK Condicionales
 // Custom
-// Condicionales
-// Opciones del PropertyPaneChoiceGroup dinamicas y property dinámicos //Mirar lo de cajas que ya empecé
-
+// Opciones del PropertyPaneChoiceGroup dinamicas (carga de lista o items de SP) https://docs.microsoft.com/es-es/sharepoint/dev/spfx/web-parts/guidance/use-cascading-dropdowns-in-web-part-properties
 
 
 export default class PersonalPropertyPaneWebPart extends BaseClientSideWebPart<IPersonalPropertyPaneWebPartProps> {
@@ -74,8 +99,31 @@ export default class PersonalPropertyPaneWebPart extends BaseClientSideWebPart<I
     { key: strings.Female, text: strings.Female },
   ];
 
+  private readonly headerTypeOptions: Array<IPropertyPaneDropdownOption> = [
+    {
+      index: 0,
+      key: strings.Option1,
+      text: strings.Option1,
+      type: PropertyPaneDropdownOptionType.Normal
+    },
+    {
+      index: 1,
+      key: strings.Option2,
+      text: strings.Option2,
+      type: PropertyPaneDropdownOptionType.Normal
+    },
+    {
+      index: 2,
+      key: strings.Option3,
+      text: strings.Option3,
+      type: PropertyPaneDropdownOptionType.Normal
+    }
+  ];
+
+
 
   public render(): void {
+
     const element: React.ReactElement<IPersonalPropertyPaneProps> = React.createElement(
       PersonalPropertyPane,
       {
@@ -89,7 +137,7 @@ export default class PersonalPropertyPaneWebPart extends BaseClientSideWebPart<I
         company: this.properties.company,
         aboutMe: this.properties.aboutMe,
         layout: this.properties.layout,
-        shape: this.properties.shape
+        shape: this.properties.shape,
       }
     );
 
@@ -107,18 +155,129 @@ export default class PersonalPropertyPaneWebPart extends BaseClientSideWebPart<I
 
   private simpleTextBoxValidationMethod(value: string): string {
     if (value.length < 3) {
-        return "Value must be more than 3 characters!";
+      return strings.ErrorValidationString;
     } else {
       return "";
     }
   }
 
+  private WPPropButtonClick(): void {
+    console.log("Botón pulsado");
+  }
+
+  private heroButtonClick(oldVal: any): any {
+    if (oldVal === "Something old") {
+      console.log("Algo");
+    }
+    return "Something new";
+  }
+
+  private ButtonClick(oldVal: any): any {
+    this.properties.textoTest = "Actualizado";
+    return "test";
+  }
+
+  private getPropertyPaneCustomConfiguration = (): Array<IPropertyPaneGroup | IPropertyPaneConditionalGroup> => {
+
+    let collectionDataGroupFields: Array<IPropertyPaneField<IPropertyPaneTextFieldProps | IPropertyPaneToggleProps | void>> = [
+      PropertyPaneDropdown('headerType', {
+        label: strings.HeaderType,
+        options: this.headerTypeOptions
+      })
+    ];
+
+    let collectionDataGroup: IPropertyPaneGroup | IPropertyPaneConditionalGroup = {
+      isCollapsed: false,
+      groupName: strings.CollectionDataGroupName,
+      groupFields: collectionDataGroupFields
+    };
+
+    let result: Array<IPropertyPaneGroup | IPropertyPaneConditionalGroup> = [
+      collectionDataGroup
+    ];
+
+    const { headerType } = this.properties;
+
+    collectionDataGroup.groupFields = collectionDataGroupFields.concat(this.createHeaderPropertyPane(headerType));
+
+    return result;
+  }
+
+
+  private createHeaderPropertyPane = (headerType: string): Array<IPropertyPaneField<IPropertyPaneTextFieldProps | IPropertyPaneToggleProps | void>> => {
+    let result: Array<IPropertyPaneField<IPropertyPaneTextFieldProps | IPropertyPaneToggleProps | void>> = [];
+
+    const commonProperties = this.getCommonProperties();
+
+    switch (headerType) {
+      case this.headerTypeOptions[0].key:
+        const sectionHeaderSimpleOnlyTitleProperties: Array<IPropertyPaneField<IPropertyPaneTextFieldProps | IPropertyPaneToggleProps | void>> = [
+          PropertyPaneToggle('enabled', {
+            label: strings.Enabled,
+            checked: true,
+            onText: strings.EnabledOnText,
+            offText: strings.EnabledOffText,
+          })
+        ];
+        result = commonProperties.concat(sectionHeaderSimpleOnlyTitleProperties);
+        break;
+      case this.headerTypeOptions[1].key:
+        const sectionHeaderSubtitleProperties: Array<IPropertyPaneField<IPropertyPaneTextFieldProps | IPropertyPaneToggleProps | void>> = [
+          PropertyPaneTextField('subTitle', {
+            label: strings.SubTitleLabel
+          })
+        ];
+        result = commonProperties.concat(sectionHeaderSubtitleProperties);
+        break;
+      case this.headerTypeOptions[2].key:
+        const sectionHeaderSimpleTitleLinkProperties: Array<IPropertyPaneField<IPropertyPaneTextFieldProps | IPropertyPaneToggleProps | void>> = [
+          PropertyPaneTextField('descriptionCustom', {
+            label: strings.DescriptionCustomLabel
+          })
+        ];
+        result = commonProperties.concat(sectionHeaderSimpleTitleLinkProperties);
+        break;
+    }
+    return result;
+  }
+
+
+  private getCommonProperties = () => {
+    let result: Array<IPropertyPaneField<IPropertyPaneTextFieldProps | IPropertyPaneToggleProps | void>> = [
+      PropertyPaneTextField('title', {
+        label: strings.TitleLabel
+      }),
+    ];
+    return result;
+  }
+
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
 
+    //Ejemplo de PropertyPaneChoiceGroup con imagenes
     const layoutBrick: string = require('./assets/brick.svg');
     const layoutGrid: string = require('./assets/grid.svg');
     const layoutCarousel: string = require('./assets/carousel.svg');
+
+    //Ejemplo de controles condicionales (Page 3)
+    let allEventsUrl: any;
+    if (this.properties.toggleAllEvents) {
+      allEventsUrl = PropertyPaneSlider('maxEvents', {
+        label: "Max events",
+        min: 1,
+        max: 10,
+        value: 1,
+        showValue: true,
+        step: 1
+      });
+    }
+    else {
+      allEventsUrl = PropertyPaneLabel('emptyLabel', {
+        text: ""
+      });
+    }
+
+
 
     return {
       pages: [
@@ -144,11 +303,7 @@ export default class PersonalPropertyPaneWebPart extends BaseClientSideWebPart<I
                 }),
                 PropertyPaneTextField('DNI', {
                   label: strings.DNIFieldLabel,
-                  maxLength: 9,
-                  // onGetErrorMessage: this.simpleTextBoxValidationMethod,
-                  // errorMessage: "This is the error message",
-                  //deferredValidationTime: 5000,
-                  //validateOnFocusOut: true
+                  maxLength: 9
                 })
               ]
             },
@@ -257,6 +412,136 @@ export default class PersonalPropertyPaneWebPart extends BaseClientSideWebPart<I
                       }
                     }
                   ]
+                })
+              ]
+            }
+          ]
+        },
+        {
+          header: {
+            description: strings.ConditionalFieldDescription
+          },
+          displayGroupsAsAccordion: false,
+          groups: [
+            {
+              groupFields: [
+                PropertyPaneDropdown('conversationSource', {
+                  label: strings.CFconversationSource,
+                  selectedKey: this.properties.conversationSource,
+                  options: [
+                    {
+                      key: "Group",
+                      text: "Group"
+                    },
+                    {
+                      key: "User",
+                      text: "User"
+                    },
+                    {
+                      key: "Topic",
+                      text: "Topic"
+                    },
+                    {
+                      key: "Home",
+                      text: "Home"
+                    },
+                  ]
+                }),
+                this.properties.conversationSource !== "Home" && PropertyPaneTextField('searchCriteria', {
+                  label: strings.CFsearchCriteria,
+                  placeholder: strings.CFsearchCriteriaPlaceHolder,
+                }),
+                PropertyPaneDropdown('numberOfConversations', {
+                  disabled: this.properties.conversationSource !== "Home",
+                  label: strings.CFnumberConversations,
+                  selectedKey: this.properties.conversationSource,
+                  options: [
+                    {
+                      key: 4,
+                      text: "Small - 4 conversations"
+                    },
+                    {
+                      key: 8,
+                      text: "Medium - 8 conversations"
+                    },
+                    {
+                      key: 12,
+                      text: "Large - 12 conversations"
+                    }
+                  ]
+                })
+              ]
+            },
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneToggle('toggleAllEvents', {
+                  key: 'toggleAllEvents',
+                  label: '',
+                  checked: false,
+                  onText: "Some text",
+                  offText: "Some other text",
+                }),
+                allEventsUrl
+              ]
+            }
+          ]
+        },
+        {
+          header: {
+            description: strings.ConditionalFieldDescriptionTwo
+          },
+          displayGroupsAsAccordion: false,
+          groups: this.getPropertyPaneCustomConfiguration()
+        },
+        {
+          header: {
+            description: strings.MyButtons,
+          },
+          displayGroupsAsAccordion: false,
+          groups: [
+            {
+              groupFields: [
+                PropertyPaneTextField('textoTest', {
+                  label: strings.ChangeMe
+                }),
+                PropertyPaneButton('btnNormal', {
+                  text: strings.BtnNormalButton,
+                  buttonType: PropertyPaneButtonType.Normal,
+                  onClick: this.ButtonClick.bind(this)
+                }),
+                PropertyPaneHorizontalRule(),
+                PropertyPaneButton('btnPrimary', {
+                  text: strings.BtnPrimaryButton,
+                  buttonType: PropertyPaneButtonType.Primary,
+                  onClick: this.WPPropButtonClick
+                }),
+                PropertyPaneHorizontalRule(),
+                PropertyPaneButton('btnHero', {
+                  text: strings.BtnHeroButton,
+                  buttonType: PropertyPaneButtonType.Hero,
+                  icon: 'Add',
+                  onClick: this.heroButtonClick,
+                }),
+                PropertyPaneHorizontalRule(),
+                PropertyPaneButton('btnCommand', {
+                  text: strings.BtnCommandButton,
+                  buttonType: PropertyPaneButtonType.Command,
+                  onClick: this.WPPropButtonClick,
+                }),
+                PropertyPaneHorizontalRule(),
+                PropertyPaneButton('btnCompound', {
+                  text: strings.BtnCompoundButton,
+                  buttonType: PropertyPaneButtonType.Compound,
+                  description: strings.BtnCompoundButtonDescripcion,
+                  onClick: this.WPPropButtonClick,
+                }),
+                PropertyPaneHorizontalRule(),
+                PropertyPaneButton('btnIcon', {
+                  text: strings.BtnIconButton,
+                  buttonType: PropertyPaneButtonType.Icon,
+                  icon: 'AddFriend',
+                  onClick: this.WPPropButtonClick
                 })
               ]
             }
--- src/webparts/personalPropertyPane/loc/mystrings.d.ts
+++ src/webparts/personalPropertyPane/loc/mystrings.d.ts
@@ -1,8 +1,7 @@
 declare interface IPersonalPropertyPaneWebPartStrings {
   // PropertyPaneDescription: string;
-  // BasicGroupName: string;
+  BasicGroupName: string;
   DescriptionFieldLabel: string;
-
   PersDataDescription: string;
   PersData_GrGeneralInfo: string;
   PersData_GrMoreInfo: string;
@@ -18,25 +17,47 @@ declare interface IPersonalPropertyPaneWebPartStrings {
   AgeMaxValue: number;
   CountryBirthLabel: string;
   LinkLabel: string;
-
-
   LaboralDataDescription: string;
   LaboralData_GrGeneralInfo: string;
   LaboralData_GrMoreInfo: string;
-
   Spain: string;
   France: string;
   EEUU: string;
   Germany: string;
-
   Male: string;
   Female: string;
-
   CompanyFieldLabel: string;
   AboutMeFieldLabel: string;
-
   Layout: string;
   Shape: string;
+  ConditionalFieldDescription: string;
+  ConditionalFieldDescriptionTwo: string;
+  CFconversationSource: string;
+  CFsearchCriteria: string;
+  CFsearchCriteriaPlaceHolder: string;
+  CFnumberConversations: string;
+  MyButtons: string;
+  Option1: string;
+  Option2: string;
+  Option3: string;
+  HeaderType: string;
+  CollectionDataGroupName: string;
+  TitleLabel: string;
+  Enabled: string;
+  EnabledOnText: string;
+  EnabledOffText: string;
+  SubTitleLabel: string;
+  DescriptionCustomLabel: string;
+  ChangeMe: string;
+  BtnNormalButton: string;
+  BtnPrimaryButton: string;
+  BtnHeroButton: string;
+  BtnCommandButton: string;
+  BtnCompoundButton: string;
+  BtnCompoundButtonDescripcion: string;
+  BtnIconButton: string;
+  label: strings;
+  ErrorValidationString: string;
 }
 
 declare module 'PersonalPropertyPaneWebPartStrings' {
